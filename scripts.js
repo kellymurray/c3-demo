6c72e10adc6cea61a659944d4d59f02e4001bbe9
Kelly Murray <kelly@theironyard.com>
2015-06-24 13:57:19 -0400

    adding onclick toggle functionality

--- scripts.js
+++ scripts.js
@@ -1,19 +1,21 @@
-var state = ['pie', 'donut'];
-var currentState = 0;
+$(function() {
 
-var chart = c3.generate({
-    data: {
-        columns: [
-            ['Gamers', 200],
-            ['Other People', 90],
-        ],
-        type : 'pie',
-        //onclick: function (d, i) { console.log("onclick", d, i); },
-        onclick: function(d, i) { chart.transform('donut'); },
-        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
-    }
+  var state = ['pie', 'donut', 'bar'];
+  var currentState = 0;
+
+  var chart = c3.generate({
+      data: {
+          columns: [
+              ['Gamers', 200],
+              ['Other People', 90],
+          ],
+          type : 'pie',
+          //onclick: function (d, i) { console.log("onclick", d, i); },
+          onclick: function(d, i) {
+            currentState = (currentState + 1) % state.length;
+            chart.transform(state[currentState]); },
+          onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+          onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+      }
+  });
 });
-//setTimeout(function () {
-    //chart.transform('donut');
-//}, 2000);
